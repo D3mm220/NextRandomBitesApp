330c5c48daf2d7ea623982045501fd6c268fa16f
DJS <demianjuarezsaldana@gmail.com>
2023-08-18 12:08:00 -0300

    error(getData.tsx, home/page.tsx): Api request dont work, dont know how to show the image

---
+++ src/app/api/GetData.tsx
@@ -0,0 +1,25 @@
+import { typesPlaceId } from "../types/typesPlaceId";
+import { typesPlaces } from "../types/typesPlaces";
+
+export const getDataNearbySearch = async (): Promise<typesPlaces> => {
+    const apiNearbySearch = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${process.env.NEXT_PUBLIC_API_KEY}&location=-34.6167431,-58.3767212&radius=10&keyword=bar`
+    const response = await fetch(apiNearbySearch)
+    const data = await response.json();
+    return data;
+};
+
+export const getDataPlaceId = async (currentId: string): Promise<typesPlaceId> => {
+    const apiPlaceId = `https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.NEXT_PUBLIC_API_KEY}&place_id=${currentId}&fields=address_components,formatted_address,name,photos,url,types`
+    const response = await fetch(apiPlaceId);
+    const data = await response.json();
+    return data;
+};
+
+export const getPlacePhoto = async (photoReference: string): Promise<any> => {
+        console.log(photoReference);
+        const apiPlacePhoto = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${photoReference}&key=${process.env.NEXT_PUBLIC_API_KEY}`
+        const response = await fetch(apiPlacePhoto)
+        console.log(response)
+        const data = await response.blob()
+        return await data;
+}
---
+++ src/app/home/page.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+import { getDataNearbySearch, getDataPlaceId, getPlacePhoto } from "../api/GetData";
+import { typesResult } from "../types/typesPlaces";
+import { Photo, placeIdResult } from "../types/typesPlaceId";
+import Image from "next/image";
+
+const Home = () => {
+
+  // typesPlaces Array
+  // Array{typesPlaces} other form
+  const [ places, setPlaces ] = useState<typesResult[]>([]) // Array of places
+  const [ currentPlace, setCurrentPlace ] = useState<typesResult>(); // Object of current place
+  const [ index, setIndex ] = useState<number>(0); // Int helper for iteration of current place
+  const [ currentId, setCurrentId ] = useState<string>("") // ID OF CURRENT PLACE
+  const [ currentPlaceId, setCurrentPlaceId ] = useState<placeIdResult>() // ARRAY OF PLACE ID
+  const [ photos, setPhotos] = useState<Photo[]>([])
+  const [ currentPhoto, setCurrentPhoto] = useState<string>("")
+  const [ fetchedPhoto, setFetchedPhoto] = useState<string>("")
+  
+  useEffect(() => {
+      const bringNearbySearch = async () => { 
+        const data = await getDataNearbySearch()
+        setPlaces(data.results)
+        setCurrentPlace(data.results[0])
+        setCurrentId(data.results[0].place_id)
+      }
+      bringNearbySearch();
+  },[])
+
+
+  useEffect(() => {
+    if (places.length > 0) {
+      setCurrentPlace(places[index])
+      setCurrentId(places[index].place_id)
+    }  
+  },[index])
+
+  useEffect(() => {
+    const bringPLaceId = async () => {
+      const data = await getDataPlaceId(currentId);
+      setCurrentPlaceId(data.result)
+      setPhotos(data.result.photos)
+      setCurrentPhoto(data.result.photos[0].photo_reference)
+    }
+    currentId !== "" && bringPLaceId()
+  },[index, currentId])
+
+  useEffect(() => {
+    const bringPhoto = async() => {
+      const data = await getPlacePhoto(currentPhoto)
+      const blob = await data.Blob()
+      setFetchedPhoto(URL.createObjectURL(blob));
+    }
+    bringPhoto()
+  },[index, currentPhoto])
+
+  const handleAnterior = () => {
+    index > 0 && setIndex(index - 1);
+  }
+
+  const handleSiguiente = () => {
+    index < 10 && setIndex(index + 1);
+  }
+
+
+  return (
+    <div>
+      <h2 className="bg-stone-500">Search Page</h2>
+      {
+        currentPlace && currentPlaceId &&
+        <div>
+          <h2 className="text-green-800">Lugar: {currentPlace.name}</h2>
+          <p className="text-blue-800">Direccion: {currentPlaceId.formatted_address}</p>
+          <Image src={fetchedPhoto} alt={fetchedPhoto} />
+          {
+            index > 0 ? <button onClick={handleAnterior} className="bg-pink-400 rounded">Anterior</button> : <button disabled className="bg-stone-400 rounded">Anterior</button>
+          }
+          {
+            index < 10 ? <button onClick={handleSiguiente} className="bg-pink-400 rounded">Siguiente</button> : <button disabled className="bg-stone-400 rounded">Siguiente</button>
+          }
+        
+        </div>
+      }
+    </div>
+  )
+}
+
+export default Home
