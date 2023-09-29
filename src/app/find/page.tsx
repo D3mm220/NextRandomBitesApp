e357545f2bd0d13b6c94a294aa40bb0ef6f39cb5
DJS <demianjuarezsaldana@gmail.com>
2023-09-29 19:52:50 -0300

    arreglado bug de que si el ultimo no anda entonces explota, hay q ver si anda con index -1 linea 86

--- src/app/find/page.tsx
+++ src/app/find/page.tsx
@@ -11,22 +11,34 @@ import { typesResult } from "@/src/types/typesPlaces";
 import { Photo, placeIdResult } from "@/src/types/typesPlaceId";
 import { Location } from "@/src/types/typesGeolocation";
 import { Card } from "@/src/components/Card";
+import { Console } from "console";
 
 const Find = () => {
   // typesPlaces Array
   // Array{typesPlaces} other form
   const [places, setPlaces] = useState<typesResult[]>([]); // Array de los lugares que devuelve getDataNearbySearch
+  console.log("🚀 ~ file: page.tsx:19 ~ Find ~ places:", places)
   const [currentPlace, setCurrentPlace] = useState<typesResult>(); // Objecto del CurrentPlace, parte del array de places
+  console.log("🚀 ~ file: page.tsx:21 ~ Find ~ currentPlace:", currentPlace)
   const [index, setIndex] = useState<number>(0); // Integer helper para la iteration del CurrentPlace
+  console.log("🚀 ~ file: page.tsx:23 ~ Find ~ index:", index)
   const [currentId, setCurrentId] = useState<string>(""); // ID del CurrentPlace
+  console.log("🚀 ~ file: page.tsx:25 ~ Find ~ currentId:", currentId)
   const [currentPlaceId, setCurrentPlaceId] = useState<placeIdResult>(); // ARRAY OF PLACE ID
+  console.log("🚀 ~ file: page.tsx:27 ~ Find ~ currentPlaceId:", currentPlaceId)
   const [photos, setPhotos] = useState<Photo[]>([]); // ARRAY OF Photos
+  console.log("🚀 ~ file: page.tsx:29 ~ Find ~ photos:", photos)
   const [indexPhoto, setIndexPhoto] = useState<number>(0);
+  console.log("🚀 ~ file: page.tsx:31 ~ Find ~ indexPhoto:", indexPhoto)
   const [currentPhoto, setCurrentPhoto] = useState<string>("");
+  console.log("🚀 ~ file: page.tsx:33 ~ Find ~ currentPhoto:", currentPhoto)
   const [fetchedPhoto, setFetchedPhoto] = useState<string>("");
+  console.log("🚀 ~ file: page.tsx:35 ~ Find ~ fetchedPhoto:", fetchedPhoto)
   const [location, setLocation] = useState<Location>(); // Coordenadas del usuario
-
-  console.log();
+  console.log("🚀 ~ file: page.tsx:37 ~ Find ~ location:", location)
+  const [lastAction, setLastAction] = useState(null);
+  console.log("🚀 ~ file: page.tsx:39 ~ Find ~ lastAction:", lastAction)
+  console.log(places.length)
 
   //trae las coordenadas
   useEffect(() => {
@@ -56,15 +68,26 @@ const Find = () => {
   useEffect(() => {
     if (places.length > 0) {
       setCurrentPlace(places[index]);
-      setCurrentId(places[index].place_id);
+      setCurrentId(places[index]?.place_id);
     }
   }, [index, places]);
 
   useEffect(() => {
     const bringPLaceId = async () => {
       const data = await getDataPlaceId(currentId);
-      if (data.result.photos === undefined) {
-        setIndex(index + 1);
+      if (data.result?.photos === undefined) {
+        if (lastAction === 'handleSiteAnterior') {
+          setIndex(index - 1);
+        } else if (lastAction === 'handleSiteSiguiente') {
+          if (index === places.length) {
+            const updatedPlaces = [...places];
+            updatedPlaces.pop();
+            setPlaces(updatedPlaces)
+            setIndex(index - 2);
+          } else {
+            setIndex(index + 1);
+          }
+        }
         return; // Sale de la función para evitar más actualizaciones innecesarias
       }
       setCurrentPlaceId(data.result);
@@ -100,13 +123,14 @@ const Find = () => {
 
   const handleSiteAnterior = () => {
     index > 0 && setIndex(index - 1);
+    setLastAction('handleSiteAnterior');
   };
 
   const handleSiteSiguiente = () => {
     index < places.length - 1 && setIndex(index + 1);
+    setLastAction('handleSiteSiguiente');
   };
 
-  console.log(currentPlaceId?.photos);
 
   return (
     <div className="bg-[#E8F9FD] max-h-screen">
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -31,9 +31,9 @@ export const Card = (props: {
 
   return (
     <div className="containerDiv h-[calc(100vh-6rem-48px)] flex justify-center items-center">
-      <div className="container flex h-[400px] w-[1000px] bg-[#FF1E00] rounded-md shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)]">
-        <div className="leftSide flex">
-          <div className="backgroundImage flex relative bg-black justify-center items-center w-[600px] h-[400px]">
+      <div className="container flex flex-row h-[600px] w-[1600px] bg-[#FF1E00] rounded-md shadow-[5px_5px_rgba(0,_98,_90,_0.4),_10px_10px_rgba(0,_98,_90,_0.3),_15px_15px_rgba(0,_98,_90,_0.2),_20px_20px_rgba(0,_98,_90,_0.1),_25px_25px_rgba(0,_98,_90,_0.05)]">
+        <div className="leftSide flex flex-col">
+          <div className="backgroundImage flex relative bg-black justify-center items-center w-[800px] h-[600px]">
             <Image
               src={fetchedPhoto}
               alt={fetchedPhoto}
@@ -42,16 +42,16 @@ export const Card = (props: {
             />
           </div>
         </div>
-        <div className="rightSide">
-          <div className="text flex flex-col h-[200px] p-[10px] text-3xl">
+        <div className="rightSide flex flex-col justify-center items-around p-4">
+          <div className="text flex flex-col h-[400px] text-4xl">
             <p className="self-start">Place: {currentPlace.name}</p>
             <p className="self-end">
               Direction: {currentPlaceId.formatted_address}
             </p>
           </div>
           {fetchedPhoto && (
-            <div className="Buttons flex flex-row flex-wrap justify-around items-center">
-              <div className="PictureButtons flex flex-row justify-between">
+            <div className="Buttons flex flex-col h-[200px] flex-wrap justify-center items-center">
+              <div className="PictureButtons flex flex-row justify-between ">
                 {indexPhoto > 0 ? (
                   <button
                     onClick={handlePhotoAnterior}
