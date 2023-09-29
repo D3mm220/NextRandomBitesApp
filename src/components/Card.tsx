96b08faa8cb876ba0f4f5743f8db4767e037daa9
DJS <demianjuarezsaldana@gmail.com>
2023-09-28 22:05:09 -0300

    more styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -55,31 +55,43 @@ export const Card = (props: {
                 {indexPhoto > 0 ? (
                   <button
                     onClick={handlePhotoAnterior}
-                    className="bg-white rounded w-20 h-16 m-4"
+                    className="bg-white rounded w-32 h-16 m-4 border-black border-solid border-2"
                   >
-                    Previous <br /> Picture
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMFJREFUSEvt1DEKwkAURdGTRQjaK7gGEdyFYC24HbEWXIyCe7CxF1yEfDCQInFmjOlMGYZ733/Mn8rAXzUw31+QbLikojE22CepjQO5goCfMcMOx1xJjiDgF0xxxwKPXwma8BtWJfAI8WmCEa7v5AFf4pmbvD7XJQh4dD7H1/CuCZrwksCtYdt+Di6I1INWVNfyE0lqD3rfpJQgpum1CzmCWlI/FVuccq9XriB4E6xxyIWnNrmE03m2ZIKvhH9BsrYXxMYgGcobjbkAAAAASUVORK5CYII=" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWJJREFUSEvV1c8rZ2EUx/GXUlZmN0MpJVP8ARYiJQs1Zcg/odipIb+2fmUa1vM/qJkUWbCQlOxsWEiUBSmlrKTo0VW3697vvX1vFs7q1nPO533Oec55bo0PtpoP1lcE0IgNdCSSOcIgrislmQf4igN8zxA5QxdusyCVAEF8D+05bTxFN+7S/OKAISwVEMy7thP8wmZwjAOu0JQXXfD8HK1JwHOF4H/4i63I5wdG8bNCzGvy8QqyALNYyBBaxFTGWSFAyHw4SmQC43jCGv5EwrvoTYEUAnTiEPOYSYiEqkJ1YRf+Vwuow2O0TA0JkRuEJfyG8J20QhXU4yEH8AX31QJ6sJ/Toj7sVAuIX/IcRlCLVawgTN42+qsFhLgwPb8zRnEZk2XG9C12PRrN0K5gAxhDWLgsK3TJBV+GVLd3gEs0l1GMxV6gJflUhNc09LStJOQY02mvaUnd9PC8P1pp6OcHvAADjkIZadn3SQAAAABJRU5ErkJggg==" />
+                    </div>
                   </button>
                 ) : (
                   <button
                     disabled
-                    className="bg-stone-400 rounded w-20 h-16 m-4"
+                    className="bg-stone-400 rounded w-32 h-16 m-4"
                   >
-                    Previous <br /> Picture{" "}
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMFJREFUSEvt1DEKwkAURdGTRQjaK7gGEdyFYC24HbEWXIyCe7CxF1yEfDCQInFmjOlMGYZ733/Mn8rAXzUw31+QbLikojE22CepjQO5goCfMcMOx1xJjiDgF0xxxwKPXwma8BtWJfAI8WmCEa7v5AFf4pmbvD7XJQh4dD7H1/CuCZrwksCtYdt+Di6I1INWVNfyE0lqD3rfpJQgpum1CzmCWlI/FVuccq9XriB4E6xxyIWnNrmE03m2ZIKvhH9BsrYXxMYgGcobjbkAAAAASUVORK5CYII=" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWJJREFUSEvV1c8rZ2EUx/GXUlZmN0MpJVP8ARYiJQs1Zcg/odipIb+2fmUa1vM/qJkUWbCQlOxsWEiUBSmlrKTo0VW3697vvX1vFs7q1nPO533Oec55bo0PtpoP1lcE0IgNdCSSOcIgrislmQf4igN8zxA5QxdusyCVAEF8D+05bTxFN+7S/OKAISwVEMy7thP8wmZwjAOu0JQXXfD8HK1JwHOF4H/4i63I5wdG8bNCzGvy8QqyALNYyBBaxFTGWSFAyHw4SmQC43jCGv5EwrvoTYEUAnTiEPOYSYiEqkJ1YRf+Vwuow2O0TA0JkRuEJfyG8J20QhXU4yEH8AX31QJ6sJ/Toj7sVAuIX/IcRlCLVawgTN42+qsFhLgwPb8zRnEZk2XG9C12PRrN0K5gAxhDWLgsK3TJBV+GVLd3gEs0l1GMxV6gJflUhNc09LStJOQY02mvaUnd9PC8P1pp6OcHvAADjkIZadn3SQAAAABJRU5ErkJggg==" />
+                    </div>
                   </button>
                 )}
                 {indexPhoto < photos.length - 1 ? (
                   <button
                     onClick={handlePhotoSiguiente}
-                    className="bg-white rounded w-20 h-16 m-4"
+                    className="bg-white rounded w-32 h-16 m-4 border-black border-solid border-2"
                   >
-                    Next <br /> Picture
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWJJREFUSEvV1c8rZ2EUx/GXUlZmN0MpJVP8ARYiJQs1Zcg/odipIb+2fmUa1vM/qJkUWbCQlOxsWEiUBSmlrKTo0VW3697vvX1vFs7q1nPO533Oec55bo0PtpoP1lcE0IgNdCSSOcIgrislmQf4igN8zxA5QxdusyCVAEF8D+05bTxFN+7S/OKAISwVEMy7thP8wmZwjAOu0JQXXfD8HK1JwHOF4H/4i63I5wdG8bNCzGvy8QqyALNYyBBaxFTGWSFAyHw4SmQC43jCGv5EwrvoTYEUAnTiEPOYSYiEqkJ1YRf+Vwuow2O0TA0JkRuEJfyG8J20QhXU4yEH8AX31QJ6sJ/Toj7sVAuIX/IcRlCLVawgTN42+qsFhLgwPb8zRnEZk2XG9C12PRrN0K5gAxhDWLgsK3TJBV+GVLd3gEs0l1GMxV6gJflUhNc09LStJOQY02mvaUnd9PC8P1pp6OcHvAADjkIZadn3SQAAAABJRU5ErkJggg==" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMJJREFUSEvt1MGJgjEYhOHHHgTvXrYHBW1H9irYiXgXLEQQaxC0AFlhW1ACCr+If5IPxYu5Jpk3M5mk482j82Z9X0A24UhEMyxxzKpTfQe/mGOPUQmk1kEPW/RxwACnNie1gKTVhOyuTp5CIoAbZI0ftEKigATpYpODtAHOJS1prElOhvhv7ns1IDXr7j4+GlFbQkX5J4GIg1TTogZFAFVvIAKYYHHt/hh/uaZFIppiVfIPRRzkDvwwH3FQBfkCsnFdABLMKBkZIo9PAAAAAElFTkSuQmCC" />
+                    </div>
                   </button>
                 ) : (
                   <button
                     disabled
-                    className="bg-stone-400 rounded w-20 h-16 m-4"
+                    className="bg-stone-400 rounded w-32 h-16 m-4"
                   >
-                    Next <br /> Picture
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAWJJREFUSEvV1c8rZ2EUx/GXUlZmN0MpJVP8ARYiJQs1Zcg/odipIb+2fmUa1vM/qJkUWbCQlOxsWEiUBSmlrKTo0VW3697vvX1vFs7q1nPO533Oec55bo0PtpoP1lcE0IgNdCSSOcIgrislmQf4igN8zxA5QxdusyCVAEF8D+05bTxFN+7S/OKAISwVEMy7thP8wmZwjAOu0JQXXfD8HK1JwHOF4H/4i63I5wdG8bNCzGvy8QqyALNYyBBaxFTGWSFAyHw4SmQC43jCGv5EwrvoTYEUAnTiEPOYSYiEqkJ1YRf+Vwuow2O0TA0JkRuEJfyG8J20QhXU4yEH8AX31QJ6sJ/Toj7sVAuIX/IcRlCLVawgTN42+qsFhLgwPb8zRnEZk2XG9C12PRrN0K5gAxhDWLgsK3TJBV+GVLd3gEs0l1GMxV6gJflUhNc09LStJOQY02mvaUnd9PC8P1pp6OcHvAADjkIZadn3SQAAAABJRU5ErkJggg==" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMJJREFUSEvt1MGJgjEYhOHHHgTvXrYHBW1H9irYiXgXLEQQaxC0AFlhW1ACCr+If5IPxYu5Jpk3M5mk482j82Z9X0A24UhEMyxxzKpTfQe/mGOPUQmk1kEPW/RxwACnNie1gKTVhOyuTp5CIoAbZI0ftEKigATpYpODtAHOJS1prElOhvhv7ns1IDXr7j4+GlFbQkX5J4GIg1TTogZFAFVvIAKYYHHt/hh/uaZFIppiVfIPRRzkDvwwH3FQBfkCsnFdABLMKBkZIo9PAAAAAElFTkSuQmCC" />
+                    </div>
                   </button>
                 )}
               </div>
@@ -87,31 +99,43 @@ export const Card = (props: {
                 {index > 0 ? (
                   <button
                     onClick={handleSiteAnterior}
-                    className="bg-white rounded w-20 h-16 m-4"
+                    className="bg-white rounded w-32 h-16 m-4 border-black border-solid border-2"
                   >
-                    Previous <br /> Place
+                    <div className="flex flex-row justify-around ">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMFJREFUSEvt1DEKwkAURdGTRQjaK7gGEdyFYC24HbEWXIyCe7CxF1yEfDCQInFmjOlMGYZ733/Mn8rAXzUw31+QbLikojE22CepjQO5goCfMcMOx1xJjiDgF0xxxwKPXwma8BtWJfAI8WmCEa7v5AFf4pmbvD7XJQh4dD7H1/CuCZrwksCtYdt+Di6I1INWVNfyE0lqD3rfpJQgpum1CzmCWlI/FVuccq9XriB4E6xxyIWnNrmE03m2ZIKvhH9BsrYXxMYgGcobjbkAAAAASUVORK5CYII=" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAShJREFUSEu91LsuBVEUxvHfiU4hKpd3kChET6HQ6DRUSolEI8QbEAqRUOqodBKVhF40vIO4VCJRIjuZEcbs2WecGavZ+8ua+f77tlZHy9Fp2V8M8JGB83xKR9cZAzxiCMN4QkrXBpxjFlvYRErXBkzgOvtrMhur9E2MUHXJO1jDGeawjfUKXcqoAoziHg8I83AfYR7TtQHhh9TrKeZ/Qf61Di4x1VDhXWE6eH3fwQsGGgIEr8Ei4A5jDQFuMV4E5MXUBCN/2j+OaBV7TbhjBQfFHfTjGWHsJV4xgrciIOglHPXijkWc5B5ldXCI5T9C9hGO+ivKAH1ZFw19qE7sYgPvKUCeX8Bxl4R5nJZ9m2oVea9JcaI+KUA37eMCM7EVpACplSfzrQM+AULROxk9XhFGAAAAAElFTkSuQmCC" />{" "}
+                    </div>
                   </button>
                 ) : (
                   <button
                     disabled
-                    className="bg-stone-400 rounded w-20 h-16 m-4"
+                    className="bg-stone-400 rounded w-32 h-16 m-4"
                   >
-                    Previous <br /> Place
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMFJREFUSEvt1DEKwkAURdGTRQjaK7gGEdyFYC24HbEWXIyCe7CxF1yEfDCQInFmjOlMGYZ733/Mn8rAXzUw31+QbLikojE22CepjQO5goCfMcMOx1xJjiDgF0xxxwKPXwma8BtWJfAI8WmCEa7v5AFf4pmbvD7XJQh4dD7H1/CuCZrwksCtYdt+Di6I1INWVNfyE0lqD3rfpJQgpum1CzmCWlI/FVuccq9XriB4E6xxyIWnNrmE03m2ZIKvhH9BsrYXxMYgGcobjbkAAAAASUVORK5CYII=" />
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAShJREFUSEu91LsuBVEUxvHfiU4hKpd3kChET6HQ6DRUSolEI8QbEAqRUOqodBKVhF40vIO4VCJRIjuZEcbs2WecGavZ+8ua+f77tlZHy9Fp2V8M8JGB83xKR9cZAzxiCMN4QkrXBpxjFlvYRErXBkzgOvtrMhur9E2MUHXJO1jDGeawjfUKXcqoAoziHg8I83AfYR7TtQHhh9TrKeZ/Qf61Di4x1VDhXWE6eH3fwQsGGgIEr8Ei4A5jDQFuMV4E5MXUBCN/2j+OaBV7TbhjBQfFHfTjGWHsJV4xgrciIOglHPXijkWc5B5ldXCI5T9C9hGO+ivKAH1ZFw19qE7sYgPvKUCeX8Bxl4R5nJZ9m2oVea9JcaI+KUA37eMCM7EVpACplSfzrQM+AULROxk9XhFGAAAAAElFTkSuQmCC" />{" "}
+                    </div>
                   </button>
                 )}
                 {index < places.length - 1 ? (
                   <button
                     onClick={handleSiteSiguiente}
-                    className="bg-white rounded w-20 h-16 m-4"
+                    className="bg-white rounded w-32 h-16 m-4 border-black border-solid border-2 "
                   >
-                    Next <br /> Place
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAShJREFUSEu91LsuBVEUxvHfiU4hKpd3kChET6HQ6DRUSolEI8QbEAqRUOqodBKVhF40vIO4VCJRIjuZEcbs2WecGavZ+8ua+f77tlZHy9Fp2V8M8JGB83xKR9cZAzxiCMN4QkrXBpxjFlvYRErXBkzgOvtrMhur9E2MUHXJO1jDGeawjfUKXcqoAoziHg8I83AfYR7TtQHhh9TrKeZ/Qf61Di4x1VDhXWE6eH3fwQsGGgIEr8Ei4A5jDQFuMV4E5MXUBCN/2j+OaBV7TbhjBQfFHfTjGWHsJV4xgrciIOglHPXijkWc5B5ldXCI5T9C9hGO+ivKAH1ZFw19qE7sYgPvKUCeX8Bxl4R5nJZ9m2oVea9JcaI+KUA37eMCM7EVpACplSfzrQM+AULROxk9XhFGAAAAAElFTkSuQmCC" />{" "}
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMJJREFUSEvt1MGJgjEYhOHHHgTvXrYHBW1H9irYiXgXLEQQaxC0AFlhW1ACCr+If5IPxYu5Jpk3M5mk482j82Z9X0A24UhEMyxxzKpTfQe/mGOPUQmk1kEPW/RxwACnNie1gKTVhOyuTp5CIoAbZI0ftEKigATpYpODtAHOJS1prElOhvhv7ns1IDXr7j4+GlFbQkX5J4GIg1TTogZFAFVvIAKYYHHt/hh/uaZFIppiVfIPRRzkDvwwH3FQBfkCsnFdABLMKBkZIo9PAAAAAElFTkSuQmCC" />
+                    </div>
                   </button>
                 ) : (
                   <button
                     disabled
-                    className="bg-stone-400 rounded w-20 h-16 m-4"
+                    className="bg-stone-400 rounded w-32 h-16 m-4"
                   >
-                    Next <br /> Place
+                    <div className="flex flex-row justify-around">
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAShJREFUSEu91LsuBVEUxvHfiU4hKpd3kChET6HQ6DRUSolEI8QbEAqRUOqodBKVhF40vIO4VCJRIjuZEcbs2WecGavZ+8ua+f77tlZHy9Fp2V8M8JGB83xKR9cZAzxiCMN4QkrXBpxjFlvYRErXBkzgOvtrMhur9E2MUHXJO1jDGeawjfUKXcqoAoziHg8I83AfYR7TtQHhh9TrKeZ/Qf61Di4x1VDhXWE6eH3fwQsGGgIEr8Ei4A5jDQFuMV4E5MXUBCN/2j+OaBV7TbhjBQfFHfTjGWHsJV4xgrciIOglHPXijkWc5B5ldXCI5T9C9hGO+ivKAH1ZFw19qE7sYgPvKUCeX8Bxl4R5nJZ9m2oVea9JcaI+KUA37eMCM7EVpACplSfzrQM+AULROxk9XhFGAAAAAElFTkSuQmCC" />{" "}
+                      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMJJREFUSEvt1MGJgjEYhOHHHgTvXrYHBW1H9irYiXgXLEQQaxC0AFlhW1ACCr+If5IPxYu5Jpk3M5mk482j82Z9X0A24UhEMyxxzKpTfQe/mGOPUQmk1kEPW/RxwACnNie1gKTVhOyuTp5CIoAbZI0ftEKigATpYpODtAHOJS1prElOhvhv7ns1IDXr7j4+GlFbQkX5J4GIg1TTogZFAFVvIAKYYHHt/hh/uaZFIppiVfIPRRzkDvwwH3FQBfkCsnFdABLMKBkZIo9PAAAAAElFTkSuQmCC" />
+                    </div>
                   </button>
                 )}
               </div>
