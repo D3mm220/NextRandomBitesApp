a68be253ba06ed362ac72abf82632a362f70ce7b
DJS <demianjuarezsaldana@gmail.com>
2023-09-30 09:32:57 -0300

    checkear si anda el fix a los lugares que no se muestran

--- src/app/find/page.tsx
+++ src/app/find/page.tsx
@@ -17,28 +17,20 @@ const Find = () => {
   // typesPlaces Array
   // Array{typesPlaces} other form
   const [places, setPlaces] = useState<typesResult[]>([]); // Array de los lugares que devuelve getDataNearbySearch
-  console.log("🚀 ~ file: page.tsx:19 ~ Find ~ places:", places)
+  console.log("🚀 ~ file: page.tsx:20 ~ Find ~ places:", places);
   const [currentPlace, setCurrentPlace] = useState<typesResult>(); // Objecto del CurrentPlace, parte del array de places
-  console.log("🚀 ~ file: page.tsx:21 ~ Find ~ currentPlace:", currentPlace)
   const [index, setIndex] = useState<number>(0); // Integer helper para la iteration del CurrentPlace
-  console.log("🚀 ~ file: page.tsx:23 ~ Find ~ index:", index)
+  console.log("🚀 ~ file: page.tsx:22 ~ Find ~ index:", index);
   const [currentId, setCurrentId] = useState<string>(""); // ID del CurrentPlace
-  console.log("🚀 ~ file: page.tsx:25 ~ Find ~ currentId:", currentId)
   const [currentPlaceId, setCurrentPlaceId] = useState<placeIdResult>(); // ARRAY OF PLACE ID
-  console.log("🚀 ~ file: page.tsx:27 ~ Find ~ currentPlaceId:", currentPlaceId)
   const [photos, setPhotos] = useState<Photo[]>([]); // ARRAY OF Photos
-  console.log("🚀 ~ file: page.tsx:29 ~ Find ~ photos:", photos)
   const [indexPhoto, setIndexPhoto] = useState<number>(0);
-  console.log("🚀 ~ file: page.tsx:31 ~ Find ~ indexPhoto:", indexPhoto)
   const [currentPhoto, setCurrentPhoto] = useState<string>("");
-  console.log("🚀 ~ file: page.tsx:33 ~ Find ~ currentPhoto:", currentPhoto)
   const [fetchedPhoto, setFetchedPhoto] = useState<string>("");
-  console.log("🚀 ~ file: page.tsx:35 ~ Find ~ fetchedPhoto:", fetchedPhoto)
   const [location, setLocation] = useState<Location>(); // Coordenadas del usuario
-  console.log("🚀 ~ file: page.tsx:37 ~ Find ~ location:", location)
   const [lastAction, setLastAction] = useState(null);
-  console.log("🚀 ~ file: page.tsx:39 ~ Find ~ lastAction:", lastAction)
-  console.log(places.length)
+  console.log("🚀 ~ file: page.tsx:39 ~ Find ~ lastAction:", lastAction);
+  console.log(places.length);
 
   //trae las coordenadas
   useEffect(() => {
@@ -76,15 +68,21 @@ const Find = () => {
     const bringPLaceId = async () => {
       const data = await getDataPlaceId(currentId);
       if (data.result?.photos === undefined) {
-        if (lastAction === 'handleSiteAnterior') {
+        if (lastAction === "handleSiteAnterior") {
+          const uptadesPlaces = [...places];
+          uptadesPlaces.splice(index, 1);
+          setPlaces(uptadesPlaces);
           setIndex(index - 1);
-        } else if (lastAction === 'handleSiteSiguiente') {
+        } else if (lastAction === "handleSiteSiguiente") {
           if (index === places.length) {
             const updatedPlaces = [...places];
-            updatedPlaces.pop();
-            setPlaces(updatedPlaces)
-            setIndex(index - 2);
+            updatedPlaces.splice(index, 1);
+            setPlaces(updatedPlaces);
+            setIndex(index - 1);
           } else {
+            const updatedPlaces = [...places];
+            updatedPlaces.splice(index, 1);
+            setPlaces(updatedPlaces);
             setIndex(index + 1);
           }
         }
@@ -123,14 +121,13 @@ const Find = () => {
 
   const handleSiteAnterior = () => {
     index > 0 && setIndex(index - 1);
-    setLastAction('handleSiteAnterior');
+    setLastAction("handleSiteAnterior");
   };
 
   const handleSiteSiguiente = () => {
     index < places.length - 1 && setIndex(index + 1);
-    setLastAction('handleSiteSiguiente');
+    setLastAction("handleSiteSiguiente");
   };
-
 
   return (
     <div className="bg-[#E8F9FD] max-h-screen">
