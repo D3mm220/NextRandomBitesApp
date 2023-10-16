60140107cd9a3576e3bcdaf0b972884993baeca1
DJS <demianjuarezsaldana@gmail.com>
2023-10-16 16:19:25 -0300

    parece tmb arreglado el tema de dataplaceid, testeado

--- src/app/api/dataplaceid/route.ts
+++ src/app/api/dataplaceid/route.ts
@@ -7,5 +7,10 @@ export const GET = async (request: Request) => {
   const apiPlaceId = `https://maps.googleapis.com/maps/api/place/details/json?key=${process.env.NEXT_PUBLIC_API_KEY}&place_id=${currentId}&fields=address_components,formatted_address,name,photos,url,types`;
   const response = await fetch(apiPlaceId);
   const data = await response.json();
-  return Response.json(data);
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  //return Response.json(data);
 };
